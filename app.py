6cc7b21dc7041adfeb44a5f5e30912c0f379738f
kingtoot-2030 <lbacount321@gmail.com>
2025-04-12 18:20:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-# سيتم وضع الكود الكامل هنا لاحقًا
+import streamlit as st
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import yfinance as yf
+
+st.title("Bitcoin Price Forecast")
+
+# تحميل البيانات من ياهو
+btc_data = yf.download("BTC-USD", period="1d", interval="1h")
+
+# رسم البيانات
+st.line_chart(btc_data['Close'])
